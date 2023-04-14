ab244845effd6dbdea6cea7358eead4d9acf1575
Dummy <onyourleftpal@gmail.com>
2023-04-14 13:40:32 +0300

    init

---
+++ payment.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 6969
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(express.json())
+
+let coin = 100
+let oddInit
+app.get('/', (req, res) => {
+  oddInit = 1 + Math.floor(Math.random() * 100) / 100
+  res.render('index', {
+    title : "hehe", 
+    coin: coin,
+    odd : oddInit
+    
+  })
+  console.log(oddInit)
+})
+
+// Guess Post
+app.post('/guess', (req, res) => {
+  coin--
+  const letters = "BINGO";
+  let bingo = letters[Math.floor(Math.random() * 5)]
+  setTimeout(() =>{
+  if(req.body.myGuess === bingo) {
+    coin = coin + 5
+  }
+    
+  }, 50)
+  res.json({letter: bingo, coin: coin})
+  console.log(bingo)
+})
+
+app.listen(PORT, console.log(`server servered at ${PORT}`))
---
+++ public/index.js
@@ -0,0 +1,129 @@
+$(document).ready(() => {
+  let lightMode = true;
+  $("#eve").text("You wanna buy human bones");
+  setTimeout(() => {
+    $("#eve").text("nevermind");
+  }, 600);
+
+  $("#sike").click(() => {
+    $(".alert").slideDown("fast");
+    setTimeout(() => {
+      $(".alert").slideUp("slow");
+    }, 3000);
+  });
+
+  $("#changeColor").click(() => {
+    if (lightMode) {
+      $("body, .nav, .card").addClass("darkMode");
+      $("body, .nav, .card").removeClass("lightMode");
+
+      $("#viewMode").attr("src", "./icons/sun.svg");
+      lightMode = false;
+    } else {
+      $("body, .nav, .card").addClass("lightMode");
+      $("body, .nav, .card").removeClass("darkMode");
+      $("#viewMode").attr("src", "./icons/moon.svg");
+
+      lightMode = true;
+    }
+  });
+
+  $("li").click((e) => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+    let count = 0;
+    const interval = setInterval(() => {
+      let t = e.target.innerText;
+      let val = t
+        .split("")
+        .map((letter, index) => {
+          if (index < count) {
+            return t[index];
+          }
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+      $("#txt").text(val);
+
+      if (count >= $("#txt").data("value").length) clearInterval(interval);
+      count = count + 1 / 2;
+    }, 30);
+  });
+
+  $("li").click((e) => {
+    let listId = e.target.id + "Page";
+    $(".content").fadeOut("fast");
+    $(`#${listId}`).fadeIn("slow");
+  });
+
+  let coin;
+  let svGuess;
+
+  $("#guessForm").submit((e) => {
+    e.preventDefault();
+    $("#status").fadeOut("fast");
+    //const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let myGuess = $("#myGuess").val();
+    $("#dispMyGuess").text(myGuess);
+    $("#dispMyGuess").removeClass("btn btn-success");
+
+    $.post(
+      "/guess",
+      {
+        myGuess: myGuess,
+      },
+      (data, status) => {
+        svGuess = data.letter;
+        coin = data.coin;
+      }
+    ).done(() => {
+      $("#myCoin").text(coin);
+      const letters = "BINGO";
+      let i = 0;
+      let dat = "";
+
+      $("#guessBtn").fadeOut();
+      const inter = setInterval(() => {
+        dat = letters[Math.floor(Math.random() * 5)];
+        $("#dispTheLett").text(dat);
+
+        if (i >= 10) clearInterval(inter);
+
+        i = i + 1 / 2;
+      }, 50);
+
+      setTimeout(() => {
+        $("#dispTheLett").text(svGuess);
+        if (myGuess == svGuess) {
+          $("#dispMyGuess").addClass("btn btn-success");
+          $("#dispTheLett")
+            .addClass("btn-success")
+            .removeClass("btn-warning btn-danger");
+          $("#status")
+            .addClass("text-success")
+            .removeClass("text-danger")
+            .html(
+              `You Won 5 🪙 .. <sub><em> *humph* don't worry i will get it back</em></sub>`
+            )
+            .fadeIn("fast");
+          $("#myCoin").text(coin + 5);
+        } else {
+          $("#dispTheLett")
+            .addClass("btn-danger")
+            .removeClass("btn-warning btn-success");
+          $("#status")
+            .addClass("text-danger")
+            .removeClass("text-success")
+            .text("You have 💩 luck ")
+            .fadeIn("fast");
+          $("#myCoin").text(coin);
+        }
+        $("#guessBtn").fadeIn();
+      }, 1100);
+    });
+  });
+
+  $(".subOptions > button").click((e) => {
+    let button = e.target;
+    $("#myGuess").val(button.innerText);
+  });
+});
